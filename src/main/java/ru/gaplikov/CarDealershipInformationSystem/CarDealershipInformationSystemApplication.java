c40df920aad8e36f995a999ef13cfcd030005389
FizMatXimik <cool.gaplikov2014@yandex.ru>
2022-10-27 17:44:14 +0300

    1.0.0

---
+++ src/main/java/ru/gaplikov/CarDealershipInformationSystem/CarDealershipInformationSystemApplication.java
@@ -0,0 +1,11 @@
+package ru.gaplikov.CarDealershipInformationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDealershipInformationSystemApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CarDealershipInformationSystemApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/gaplikov/CarDealershipInformationSystem/CarDealershipInformationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gaplikov.CarDealershipInformationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDealershipInformationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
